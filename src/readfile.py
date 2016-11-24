56824c8f33a38400f8014cb3882f3863f1e73c0b
fitrakun <fitrakun@gmail.com>
2016-11-23 23:59:44 -0800

    change readfile

--- src/readfile.py
+++ src/readfile.py
@@ -1,7 +1,7 @@
-# import mysql.connector
+import mysql.connector
 import json
 
-# from mysql.connector import Error
+from mysql.connector import Error
 
 def insertTweetResult(data, calon):
   #0 : kata
@@ -16,7 +16,7 @@ def insertTweetResult(data, calon):
             print('Connected to MySQL database')
 
             for datum in data:
-              cursor.execute("INSERT INTO tweet_result (calon, kata, jumlah_kemunculan) VALUES (\'" + calon + "\',\'" + datum[0] + "\'," + str(datum[2]) + ")")
+              cursor.execute("INSERT INTO tweet_result (calon, kata, jumlah_kemunculan) VALUES (\'" + calon + "\',\'" + datum[0] + "\'," + str(datum[1]) + ")")
               conn.commit()
             print("Finish add to database")
         conn.close()
@@ -25,11 +25,12 @@ def insertTweetResult(data, calon):
 
 
 if __name__ == "__main__":
-	filename = 'ahok_result.txt'
-	data = []
-	with open(filename, 'r') as the_file:
-		for line in the_file:
-			word = line.split(',')
-			word[1].replace('/n','')
-			word2 = (word[0],int(word[1]))
-			print word2
+  filename = 'ahok_result.txt'
+  data = []
+  with open(filename, 'r') as the_file:
+    for line in the_file:
+      word = line.split(',')
+      word[1].replace('/n','')
+      word2 = (word[0],int(word[1]))
+      data.append(word2)
+  insertTweetResult(data, "AHOK")
